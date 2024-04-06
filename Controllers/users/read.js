82ac359547a1cde7b1b274405ccc27aa23ad47c7
Dhruvi Dobariya <dhruvi.dobariya@zuru.com>
2024-04-06 13:14:33 +0530

    backend data

--- Controllers/users/read.js
+++ Controllers/users/read.js
@@ -5,8 +5,24 @@ const read = async (req, res, next) => {
   const id = req.params.userId;
   console.log(id);
   const user = await User.findById(id);
-  console.log(user);
-  res.json(user);
+
+  const followersData = await Promise.all(user.followers.map(async (followerId) => {
+    const follower = await User.findById(followerId);
+    return { [followerId]: follower };
+  }));
+
+  const followingData = await Promise.all(user.following.map(async (followerId) => {
+    const follower = await User.findById(followerId);
+    return { [followerId]: follower };
+  }));
+  const userDataWithFollowers = {
+    ...user.toObject(), // Convert user to plain JavaScript object
+    followersData: followersData,
+    followingData: followingData,
+  };
+
+  console.log(userDataWithFollowers);
+  res.json(userDataWithFollowers);
 };
 
 module.exports = read;
